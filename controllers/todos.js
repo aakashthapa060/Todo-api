4a9dec2b016ead71d5c267f3b535af7f39fd459a
Aakash Thapa <aakashmanm3@gmail.com>
2022-06-26 16:35:43 +0545

    Project Init

---
+++ controllers/todos.js
@@ -0,0 +1,22 @@
+const getAllTodos = (req,res) => {
+	res.send("Getting All The Todos");
+}
+
+const getParticularTodo = async (req,res) => {
+
+}
+
+const deleteTodo= async (req,res) => {
+
+}
+
+const updateTodo = async (req,res) => {
+
+}
+
+module.exports = {
+	getAllTodos,
+	getParticularTodo,
+	deleteTodo,
+	updateTodo
+}
